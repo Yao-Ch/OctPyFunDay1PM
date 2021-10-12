89fed1e64277d0533ea0a1b7c265276ab3a1b10a
jpfnice <jpfnice@gmail.com>
2021-10-12 17:09:02 +0200

    Add files via upload

---
+++ NoneType1.py
@@ -0,0 +1,7 @@
+
+nb=12
+result=print("Nb is", nb)
+print(result, type(result))
+
+if result == None:
+    print("Ok")
---
+++ tuple1.py
@@ -0,0 +1,24 @@
+
+#Creation:
+    
+t1=(23,45,56,False,"abc")
+print(t1, type(t1))
+
+t1=23,45,56,False,"abc"
+print(t1, type(t1))
+
+#bis=t1 # bis and t1 are 2 names that give access to the same tuple
+
+data=[23,56,78]
+
+t2=tuple(data)
+print(t2, type(t2))
+
+t3=(12,)
+print(t3, type(t3))
+
+# len() in for [] [:]
+print(t1[3])
+
+
+
